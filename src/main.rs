b57b91be7e92e3dcfd5693f4ac3e7397f2d0bf7b
Adrians Uvis Valdmanis <adrians.uvis@gmail.com>
2025-01-16 15:13:51 +0200

    Set up clap and reading files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use clap::{Parser, Subcommand};
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+use std::process::exit;
+
+/// Simple program to greet a person
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Tokenize {
+        #[arg(short, long)]
+        file: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Tokenize { file }) => {
+            let mut file = match File::open(file) {
+                Ok(file) => file,
+                Err(e) => {
+                    eprintln!("Error opening file: {}", e);
+                    exit(1);
+                }
+            };
+            let mut file_contents = String::new();
+            match file.read_to_string(&mut file_contents) {
+                Ok(_) => {
+                    println!("{}", file_contents);
+                }
+                Err(e) => {
+                    eprintln!("Error reading file: {}", e);
+                    exit(1);
+                }
+            }
+        }
+        None => {
+            eprintln!("No command provided");
+            exit(1);
+        }
+    }
 }
